ca6f2f12b6a378aafd55afb82414073668e9b161
Poveda <julianopoveda@github.com>
2020-06-03 08:43:05 -0300

    Projeto azurefunctions
    Gatilho com http request.
    Classe de taxa implementada
    Método calcular taxa juros implementada

---
+++ Financeiro/Taxas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financeiro
+{
+    public class Taxas
+    {
+        //https://www3.bcb.gov.br/CALCIDADAO/publico/exibirMetodologiaFinanciamentoPrestacoesFixas.do?method=exibirMetodologiaFinanciamentoPrestacoesFixas
+        public decimal CalcularTaxaJuros(decimal valorFinanciamento, decimal parcelaDesejada, int prazoEmMeses, double margemErro = 0.000001)
+        {
+            double taxaJuros = margemErro; //O calculo inicia sempre com a margem de erro
+            double taxaJurosSimulada = 1;
+
+            //O cálculo não prevê taxa de juros 0
+            if (valorFinanciamento == parcelaDesejada * prazoEmMeses)
+                return 0;
+
+            while (Math.Abs(taxaJuros - taxaJurosSimulada) > margemErro && taxaJuros > double.MinValue)
+            {
+                taxaJurosSimulada = taxaJuros;
+
+                double taxaCorrecao = Math.Pow(1.0 + taxaJurosSimulada, -prazoEmMeses);
+
+                double funcaoTaxaJuros = (1 - Math.Pow(1.0 + taxaJurosSimulada, -prazoEmMeses)) * ((double)parcelaDesejada / taxaJurosSimulada) - (double)valorFinanciamento;
+                double derivadaFuncaoTaxaJuros = (double)parcelaDesejada * ((prazoEmMeses * (Math.Pow(1 + taxaJurosSimulada, -prazoEmMeses - 1))) / taxaJurosSimulada - (1 - Math.Pow(1 + taxaJurosSimulada, -prazoEmMeses)) / Math.Pow(taxaJurosSimulada, 2));
+                taxaJuros = taxaJurosSimulada - (funcaoTaxaJuros / derivadaFuncaoTaxaJuros);
+            }
+
+            return Math.Round(Convert.ToDecimal(taxaJuros) * 100, 6, MidpointRounding.AwayFromZero);
+        }
+
+        public decimal TIR()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
